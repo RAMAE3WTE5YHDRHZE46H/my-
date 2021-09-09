17851c215e463e58f03118df1061c2233408f789
RAMAE3WTE5YHDRHZE46H <88085265+RAMAE3WTE5YHDRHZE46H@users.noreply.github.com>
2021-09-09 12:45:46 +0530

    Add files via upload

---
+++ javaObjects.js
@@ -0,0 +1,12 @@
+var student = {
+    name: "Pran",
+    class: 10,
+    roll_no: 20,
+    favourite_subject: 'science',
+    marks: [45, 34, 46, 48]
+}
+
+function setup(){
+    student.name;
+    student.class;
+}
